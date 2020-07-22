bc454247e529c2638eb682244ada163ef236edc5
Jake Peterson <jpeterson0514@gmail.com>
2020-07-22 00:16:27 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -4,3 +4,4 @@ if run_code_inside
   puts "code inside"
 end
 puts "code after if...end"
+chance_of_rain =0.2 puts "Let's go outside!" if chance_of_rain>0.5 puts "pack an umbrella!" else puts "enjoy the fine day!" end puts "oh, and always wear sunscreen!"
