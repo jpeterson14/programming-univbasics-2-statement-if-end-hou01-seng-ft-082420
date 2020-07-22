fffe9955d3fe92d20d86755ced31b63d4c742940
Jake Peterson <jpeterson0514@gmail.com>
2020-07-22 00:33:20 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,10 +1,3 @@
-run_code_inside = false 
-puts "code before if...end"
-if run_code_inside
-  puts "code inside"
-end
-puts "code after if...end"
-chance_of_rain =0.2 puts "Let's go outside!" if chance_of_rain>0.5 puts "pack an umbrella!" else puts "enjoy the fine day!" end puts "oh, and always wear sunscreen!"
 current_time = Time.now
 current_time = current_time.to_i
 if current_time.even? 
