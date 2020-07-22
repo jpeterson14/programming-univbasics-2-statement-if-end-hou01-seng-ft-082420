1766d5acac3b8cd3606dd24fbb628bcfe2608e04
Jake Peterson <jpeterson0514@gmail.com>
2020-07-22 00:29:19 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -5,3 +5,6 @@ if run_code_inside
 end
 puts "code after if...end"
 chance_of_rain =0.2 puts "Let's go outside!" if chance_of_rain>0.5 puts "pack an umbrella!" else puts "enjoy the fine day!" end puts "oh, and always wear sunscreen!"
+current_time = Time.now
+current_time = current_time.to_i
+if current_time.even? puts "Even!" else puts "Odd!" end
